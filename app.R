986854caf45b4bab09b35707c6462d4606544418
Pranav Rajaram <53154693+pranavrajaram@users.noreply.github.com>
2025-06-24 18:59:48 -0400

    updated app

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(gganimate)
 library(arrow)
 library(shinycssloaders)
 library(tidyverse)
+library(sportyR)
 
 data_directory <- "~/SMT-Data-Challenge-2025"
 
@@ -13,8 +14,9 @@ ui <- fluidPage(
   p('Animations take about 10 seconds to load.'),
   sidebarLayout(
     sidebarPanel(
-      textInput("game_str", "Enter game_str:", value = "y1_d049_JZK_RZQ"),
-      numericInput("play_id", "Enter play_id:", value = 135, min = 1)
+      textInput("game_str", "Enter game_str:", value = "y1_d006_JJS_YJD"),
+      numericInput("play_id", "Enter play_id:", value = 163, min = 1),
+      actionButton("animate_btn", "Animate")
     ),
     mainPanel(
       withSpinner(imageOutput("play_anim"), type = 1)
@@ -24,24 +26,23 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  output$play_anim <- renderImage({
-    
-    # load data
-    player_pos <- open_csv_dataset(paste0(data_directory,"/player_pos"),
+  animation_data <- eventReactive(input$animate_btn, {
+    # Load data
+    player_pos <- open_csv_dataset(paste0(data_directory, "/player_pos"),
                                    partitioning = c("HomeTeam", "AwayTeam", "Season", "Day"),
                                    col_names = c("game_str", "play_id", "timestamp",
                                                  "player_position", "field_x", "field_y"),
                                    hive_style = FALSE, unify_schemas = TRUE,
                                    na = c("", "NA", "NULL", NA, "\\N"))
     
-    ball_pos <- open_csv_dataset(paste0(data_directory,"/ball_pos"),
+    ball_pos <- open_csv_dataset(paste0(data_directory, "/ball_pos"),
                                  partitioning = c("HomeTeam", "AwayTeam", "Season", "Day"),
                                  col_names = c("game_str", "play_id", "timestamp",
                                                "ball_position_x", "ball_position_y", "ball_position_z"),
                                  hive_style = FALSE, unify_schemas = TRUE,
                                  na = c("", "NA", "NULL", NA, "\\N"))
     
-    # filter data based on user input
+    # Filter data based on user input
     plot_df <- player_pos %>%
       filter(game_str == input$game_str, play_id == input$play_id) %>%
       collect()
@@ -69,16 +70,14 @@ server <- function(input, output, session) {
                   select(timestamp, ball_position_x, ball_position_y),
                 by = "timestamp")
     
-    # animation
-    p <- ggplot() +
-      geom_arc_bar(aes(x0 = 0, y0 = 0, r0 = 0, r = 150, start = -pi/4, end = pi/4),
-                   fill = "tan", color = "brown", inherit.aes = FALSE) +
-      geom_segment(aes(x = 0, y = 0, xend = 250, yend = 250), color = "black") +
-      geom_segment(aes(x = 0, y = 0, xend = -250, yend = 250), color = "black") +
-      geom_point(aes(x = 0, y = 0), size = 4, shape = 15) +
-      geom_point(aes(x = 63.64, y = 63.64), size = 4, shape = 15) +
-      geom_point(aes(x = 0, y = 127), size = 4, shape = 15) +
-      geom_point(aes(x = -63.64, y = 63.64), size = 4, shape = 15) +
+    return(plot_df)
+  })
+  
+  output$play_anim <- renderImage({
+    req(animation_data())
+    plot_df <- animation_data()
+    
+    p <- geom_baseball(league = "MiLB") +
       geom_point(data = plot_df, aes(x = ball_position_x, y = ball_position_y),
                  color = "gray36", size = 4, shape = 21, stroke = 1.5, fill = "gray83") +
       geom_point(data = plot_df, aes(x = field_x, y = field_y, color = side), size = 3) +
@@ -89,7 +88,6 @@ server <- function(input, output, session) {
       transition_states(timestamp, transition_length = 1, state_length = 1) +
       ease_aes('linear')
     
-    # save to temp file and render
     outfile <- tempfile(fileext = ".gif")
     anim_save(outfile, animate(p, fps = 10, width = 800, height = 600))
     
